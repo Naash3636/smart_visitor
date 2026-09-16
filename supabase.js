8c4360e27cc0c11323ba3af5a2a285fc7d973078
Naash3636 <sheneswara3636@gmail.com>
2026-09-16 10:11:33 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://fhtqjbradaxexvsqqzpi.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_S1tas0sZCNSXaekmzX2jIA_-xC1xH41";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);

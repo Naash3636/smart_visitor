35988ac9017ee8e4e9d8fac79355565bdc2713ed
Naash3636 <sheneswara3636@gmail.com>
2026-09-16 10:19:00 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://fhtqjbradaxexvsqqzpi.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://fhtqjbradaxexvsqqzpi.supabase.co";
 const SUPABASE_KEY = "sb_publishable_S1tas0sZCNSXaekmzX2jIA_-xC1xH41";
 
 const supabaseClient = window.supabase.createClient(
